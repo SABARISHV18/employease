a1a07e55dfc6135af478d5d3d9d0d652f06cdd03
SABARISH V <115387031+SABARISHV18@users.noreply.github.com>
2024-02-05 09:48:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ const Categorymodel=require('./models/Categorymodel')
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+ {
+  origin: ["https://deploy-mern-1whq.vercel.app"],
+  methods: ["POST", "GET", "PUT"],
+  credentials: true
+ }
+));
 
 
  mongoose.connect("mongodb+srv://sabarishv:Sabarii8@cluster0.ft5iy6n.mongodb.net/?retryWrites=true&w=majority");
